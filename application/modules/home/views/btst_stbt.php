5025efa24ed90d013666d2c0f8d1eadf68c45431
pawan sen <pawansen9770@gmail.com>
2017-09-15 22:25:39 +0530

    'update'

---
+++ application/modules/home/views/btst_stbt.php
@@ -0,0 +1,43 @@
+    <!--Main container sec start-->
+    <div class="main_container">
+      <section class="innerpage_banner">
+        <img src="<?php echo base_url(); ?>frontend_asset/images/about_banner.jpg" class="img-responsive">
+        <div class="innr_bnr_cap">
+          <div class="container">
+            <div class="row">
+              <div class="col-sm-6">
+                <h1 class="inr_banner_head">BTST STBT</h1>
+              </div>
+              <div class="col-sm-6">
+                <div class="bredcrumb_main">
+                  <ol class="breadcrumb">
+                    <li><a href="<?php echo base_url(); ?>">Home</a></li>
+                    <li class="active">BTST STBT</li>
+                  </ol>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="about_sec sec_padd">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12 col-sm-8">
+<!--              <div class="since_text">Since 2007</div>-->
+              <div class="head_commen">
+            <!--     <h2>Our Company History</h2> -->
+              </div>
+              <p class="pera_bottom_20">Equity Research Panel is an Investment Advisory Company which provides tips and recommendations in Stock all Segment traded in NSE & BSE, and also provide in Commodities Segment. Our team consists of highly qualified analysts who are skilled and impeccable in their analysis. These analyst, using their experience and latest software tools, are able to predict the movements in share market on time and with high accuracy. As a result, using our tips, our clients gain the most out of the share market.</p>
+              <p class="pera_bottom_20"> 
+The overall business model created keeping in mind that can provide effective Research based advice to create a complete Investment solution platform of unlimited Trading & wealth creating opportunities. This platform combines certain and enables the investors to provide a wide variety of investment solutions to gain better returns.</p>
+
+            </div>
+      
+          </div>
+        </div>
+      </section>
+     
+    </div>
+    <!--Main container sec end-->
